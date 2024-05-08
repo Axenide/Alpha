7ea79185a6e3749437b26d718ba389aed5faace6
Axenide <axenide@gmail.com>
2024-05-08 05:39:31 -0300

    Installer and stuff

--- alpha.sh
+++ alpha.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-cd $(dirname $(realpath $0))
+# If parameter is "start" then run streamlit
+# If parameter is "kill" then killall ax_alpha
+# If none of the above, then print usage message
 
-source venv/bin/activate
-
-streamlit run alpha.py --server.port 3141
+if [ "$1" = "start" ]; then
+  cd $(dirname $(realpath $0))
+  source venv/bin/activate
+  streamlit run alpha.py --server.port 3141 --server.baseUrlPath /Alpha > /dev/null &
+  clear
+  echo -e "\e[1m\e[31m🔥 Alpha\e[0m\e[93m started! Now you can access it at \e[1m\e[31mhttp://localhost:3141/Alpha\e[0m\e[93m"
+  echo ""
+elif [ "$1" = "kill" ]; then
+  killall streamlit
+else
+  echo "Usage:
+  start - Initializes Alpha.
+  kill - This, uh... Tells Alpha to go to sleep... Yeah, that's right."
+fi
--- install.sh
+++ install.sh
@@ -13,7 +13,12 @@ fi
 source venv/bin/activate
 
 # Install requirements
-pip install -r requirements.txt --quiet
+pip install -r requirements.txt
 
 # Exit the virtual environment
 deactivate
+
+clear
+
+echo -e "\e[1m\e[31mAlpha\e[0m\e[93m is now available! Use the command '\e[1m\e[31malpha\e[0m\e[93m' to start.\e[0m"
+echo ""
